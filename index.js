2bbdb3a499916f50e5cbdb0ad911f07145f70199
pixelhijack <pothattila@gmail.com>
2017-08-30 17:47:28 +0200

    [WEBHOOK] Simple echo webhook working

--- index.js
+++ index.js
@@ -20,11 +20,44 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
 });
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging;
+    console.log('[POST /webhook], ', JSON.stringify(messaging_events, null, 2));
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i];
+	    let sender = event.sender.id;
+	    if (event.message && event.message.text) {
+		    let text = event.message.text;
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+	    }
+    }
+    res.sendStatus(200)
+});
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token: process.env.VERIFY_TOKEN},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
